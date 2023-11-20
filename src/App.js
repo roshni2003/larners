30ece403b6945a23c8efe84f3422f75dad85fccb
nikita verma <nikitaverma21@navgurukul.org>
2023-11-20 17:56:19 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './header/Header'
+import About from './header/About'
+
+
+
 function App() {
   return (
     <div className="App">
       <Header/>
+      <About/>
     </div>
   );
 }

2db0f7b4ac8e4cea9e5214a482b1b53f10bd9097
Roshni Jha <roshnijha21@navgurukul.org>
2023-11-24 19:27:46 +0530

    slogan

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 
 import React from "react";
-import Registration from './Registration';
-import Login from './login';
-import Forgetpassword from './forgetpassword';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from "./header/Home"
+// import Registration from './Registration';
+// import Login from './login';
+// import Forgetpassword from './forgetpassword';
+// import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <Home/>
+      {/* <BrowserRouter>
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="registration" element={<Registration />} />
           <Route path="reset-password" element={<Forgetpassword />} />
         </Routes>
-      </BrowserRouter>
+      </BrowserRouter> */}
     </>
   );
 }

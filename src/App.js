59a1feff31485241330d42abbe53d4f6237cceef
Roshni Jha <roshnijha21@navgurukul.org>
2023-12-12 19:26:57 +0530

    Feedback Updated

--- src/App.js
+++ src/App.js
@@ -2,9 +2,7 @@
 import React from 'react';
 import './App.css';
 import Routing from './Routing'
-import Home from './header/Home';
-import StudentProfile from './student/StudentProfile';
-import Footer from './Footer/Footer';
+
 
 function App() {
   return (
@@ -12,13 +10,7 @@ function App() {
     <>
 
       <Routing />
-      <Home />
-<<<<<<< HEAD
-=======
-      <Footer />
->>>>>>> e6c490e645bdfc48d2b23bf5be8f15a8f87b1bf8
-      <StudentProfile/>
-      <Footer />
+      
     </>
 
   );
--- src/FeedbackData.js
+++ src/FeedbackData.js
@@ -1,4 +1,3 @@
-
 import React, { useEffect, useState } from 'react';
 import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
 
@@ -50,5 +49,3 @@ const FeedbackData = () => {
 };
 
 export default FeedbackData;
-
- 
